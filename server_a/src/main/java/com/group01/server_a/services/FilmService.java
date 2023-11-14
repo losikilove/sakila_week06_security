beb0288ffcbdd6255088869ee0f1d94104b1295d
losikilove <75561119+losikilove@users.noreply.github.com>
2023-11-14 16:04:15 +0700

    client can call API to server A to show list of actors and list of films (which include films of server A and films of server B)

--- server_a/src/main/java/com/group01/server_a/services/FilmService.java
+++ server_a/src/main/java/com/group01/server_a/services/FilmService.java
@@ -3,6 +3,7 @@ package com.group01.server_a.services;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.group01.server_a.models.Film;
 import com.group01.server_a.repositories.FilmRepository;
@@ -16,6 +17,19 @@ public class FilmService {
     }
 
     public List<Film> getAll() {
-        return filmRepository.getAllFilms();
+        try {
+            List<Film> listFilms = filmRepository.getAllFilms();
+            String url = "http://localhost:8082/v2/api/film/get-all";
+            RestTemplate restTemplate = new RestTemplate();
+            Film[] listRestFilms = restTemplate.postForObject(url, null, Film[].class);
+
+            for (Film film : listRestFilms) {
+                listFilms.add(film);
+            }
+
+            return listFilms;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
